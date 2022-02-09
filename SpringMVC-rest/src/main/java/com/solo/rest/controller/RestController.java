9c3ef84d4f72feed4e2f6f2ba15b25756f1a8bc1
BAILINHAN <1105629571@qq.com>
2022-02-09 23:03:12 +0800

    restful finish

--- SpringMVC-rest/src/main/java/com/solo/rest/controller/RestController.java
+++ SpringMVC-rest/src/main/java/com/solo/rest/controller/RestController.java
@@ -1,15 +1,30 @@
 package com.solo.rest.controller;
 
+import com.solo.rest.bean.Employee;
 import com.solo.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 @Controller
 public class RestController {
 
     @Autowired
     private EmployeeDao employeeDao;
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployee(Model model){
+
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList", employeeList);
+
+        return "employee_list";
+    }
+
 
 
 
